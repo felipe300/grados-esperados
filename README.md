b2c378a476135b1dcd4a43418c6be8c1eb0fde29
Felipe300 <caracoloro@gmail.com>
2023-05-04 14:49:01 -0400

    chore: initial commit

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# grados-esperados
+# Grados esperados
+
+EJERCICIO:
+La lógica de este código es permitirle al usuario ingresar un valor de temperatura en grados Fahrenheit, y
+hacer que el programa convierta el valor por medio de una función async/await. Esto significa que
+también tendrás que crear una Promesa. Como recomendación, intenta hacer todos los cálculos de
+conversión en la Promesa, y luego puedes llamarla en la función async.
+La fórmula para convertir grados Fahrenheit a grados Celsius, es la siguiente: C = (5/9) * (F - 32).
+
+![grados](./assets/img/grados-esperados.png)
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,27 @@
-console.log('testing grados-esperados!')
+let btn = document.getElementById("btn")
+
+function calculate (value) {
+	return Math.floor(((value - 32) / 1.8) * 10) / 10
+}
+
+function temperatureConverter (valNum) {
+	valNum = parseFloat(valNum)
+	let response = document.getElementById("response")
+	const promise = new Promise(function (resolve, reject) {
+		setTimeout(() => {
+			resolve(calculate(valNum))
+		}, 3000)
+	})
+
+	promise
+		.then(function (value) {
+			response.innerHTML = `${value}°C`
+		})
+		.catch(err => console.log(err))
+}
+
+btn.addEventListener("click", (e) => {
+	e.preventDefault()
+	let temp = document.getElementById("temp")
+	temperatureConverter(temp.value)
+})
